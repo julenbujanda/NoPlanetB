78634442b144dc352d3a3ec9194cfa7302e99c3e
julenb <21701719@live.uem.es>
2019-03-06 17:28:34 +0100

    Animations

--- app/src/main/java/com/julenbujanda/noplanetb/SplashScreenActivity.java
+++ app/src/main/java/com/julenbujanda/noplanetb/SplashScreenActivity.java
@@ -1,11 +1,15 @@
 package com.julenbujanda.noplanetb;
 
 import android.graphics.drawable.Drawable;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
@@ -16,6 +20,7 @@ public class SplashScreenActivity extends AppCompatActivity {
 
     private LinearLayout linearLayout;
     private ImageView ivIcon;
+    private TextView txtTitle, txtSubtitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +28,13 @@ public class SplashScreenActivity extends AppCompatActivity {
         setContentView(R.layout.activity_splash_screen);
         linearLayout = findViewById(R.id.splash_linear_layout);
         ivIcon = findViewById(R.id.iv_splash_icon);
+        txtTitle = findViewById(R.id.txt_splash_title);
+        txtSubtitle = findViewById(R.id.txt_splash_subtitle);
         loadImages();
+        mostrarAnimaciones();
+/*        new Handler().postDelayed(() -> {
+
+        }, 3000);*/
     }
 
     private void loadImages() {
@@ -39,6 +50,16 @@ public class SplashScreenActivity extends AppCompatActivity {
                 .load(R.drawable.rocketlogo)
                 .apply(RequestOptions.circleCropTransform())
                 .into(ivIcon);
+    }
+
+    private void mostrarAnimaciones() {
+        Animation slideDown = AnimationUtils.loadAnimation(this, R.anim.slide_down);
+        Animation fadeIn = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+        txtTitle.startAnimation(slideDown);
+        ivIcon.startAnimation(fadeIn);
+        ivIcon.setVisibility(View.VISIBLE);
+        txtSubtitle.startAnimation(fadeIn);
+        txtSubtitle.setVisibility(View.VISIBLE);
     }
 
 }
